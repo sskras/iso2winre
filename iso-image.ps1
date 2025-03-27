dae580528f5c6e23d0367e2f2b1eeb853cd13366
Saulius Krasuckas <saulius2@ar-fi.lt>
2025-03-27 18:06:50 +0100

    iso-image.ps1: Improve the PSh cmd line
    ... so it load less things on startup, and stays more independent.

--- iso-image.ps1
+++ iso-image.ps1
@@ -1,4 +1,4 @@
-#! /usr/bin/env -S powershell -file
+#! /usr/bin/env -S powershell -NoProfile -File
 
 # SPDX-License-Identifier: BlueOak-1.0.0
 # SPDX-FileCopyrightText: 2025 Saulius Krasuckas <saulius2_at_ar-fi_point_lt> | sskras
