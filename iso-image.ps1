ac180edf3fd9326c6e7cf5e9f81c1d9c1fe0c78e
Saulius Krasuckas <saulius2@ar-fi.lt>
2025-03-27 18:01:56 +0100

    iso-image.ps1: License header and description of the script

--- iso-image.ps1
+++ iso-image.ps1
@@ -1,1 +1,10 @@
 #! /usr/bin/env -S powershell -file
+
+# SPDX-License-Identifier: BlueOak-1.0.0
+# SPDX-FileCopyrightText: 2025 Saulius Krasuckas <saulius2_at_ar-fi_point_lt> | sskras
+
+# ISO image helper tool for:
+#
+# - checking image details,
+# - mounting an image,
+# - cleaning up virtual drives mounted from an ISO image.
